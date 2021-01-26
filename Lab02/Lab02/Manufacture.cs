a6d8be6c6ec61cfb485b146ee3b3be7ce0294e45
CTTDuy <66402413+CTTDuy@users.noreply.github.com>
2021-01-26 16:18:19 +0700

    Add files via upload

---
+++ Lab02/Lab02/Manufacture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    [Table"Manufacture"]
+    class Manufacture
+    {
+        [Key]
+        public string id { get; set; }
+        [Required]
+        [StringLength(128, MinimumLength = 3)]
+        public string name { get; set; }
+        public string location { get; set; }
+        public string employee { get; set; }
+        public virtual ICollection<Phone> phones { get; set; }
+    }
+}
---
+++ Lab02/Lab02/MarketingContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    public class MarketingContex:DbContext
+    {
+        public DbSet<Phone> phones { get; set; }
+        public DbSet<Manufacture> manufactures { get; set; }
+    }
+}
